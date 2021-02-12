11cc195396cccb7e6743a5cefb7d9ca89647f2d0
Kokinz <nes-t@mail.ru>
2021-02-12 06:16:58 +0000

    fix for linter

--- js/main.js
+++ js/main.js
@@ -6,10 +6,13 @@ const getRandomNumber = (min = 0, max = 100) => {
       ? Math.round(Math.random() * (max - min) + min)
       : Math.round(Math.random() * (min - max) + max);
   }
-  
+
   throw new Error('Диапазон меньше нуля');
 }
 
 const checkLengthOfString = (string, maxLength = 140) => {
   return string.length <= maxLength;
 }
+
+getRandomNumber ();
+checkLengthOfString ();
