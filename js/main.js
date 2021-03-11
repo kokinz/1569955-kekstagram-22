613db1172db0de31b6eb8fafc5d4a3ab941ec07d
kokinz <nes-t@mail.ru>
2021-03-11 15:33:08 +0300

    some

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {checkLengthOfString} from './util.js';
 import {renderPicures} from './pictures.js';
 import {bigPictureOpen, onPictureOpenClick, onPictureEnterKeydown} from './picture-modal.js';
+import './upload-modal.js';
 
 checkLengthOfString ('');
 renderPicures();
---
+++ js/upload-modal.js
@@ -0,0 +1,5 @@
+const uploadButton = document.querySelector('#upload-file');
+console.log(uploadButton);
+uploadButton.addEventListener('change', () => {
+  console.log(uploadButton.value);
+});
