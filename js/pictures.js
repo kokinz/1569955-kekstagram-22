c6b47ef4709829028e1cff373149efab9b361d3e
kokinz <nes-t@mail.ru>
2021-03-17 10:30:21 +0300

    sort filters click

--- js/pictures.js
+++ js/pictures.js
@@ -1,10 +1,11 @@
+const picturesSortFiltersBlock = document.querySelector('.img-filters');
+const picturesSortFiltersForm = document.querySelector('.img-filters__form');
 const pictures = [];
 let gallery = [];
 
 const renderPicures = (array) => {
   const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   const picturesContainer = document.querySelector('.pictures');
-
   const picturesFragment = document.createDocumentFragment();
   
   array.forEach(element => pictures.push(element));
@@ -22,6 +23,42 @@ const renderPicures = (array) => {
   picturesContainer.appendChild(picturesFragment);
 
   gallery = document.querySelectorAll('a.picture');
+  
+  picturesSortFiltersBlock.classList.remove('img-filters--inactive');
+  
+  addSortFiltersListener();
 }
 
-export {pictures, gallery, renderPicures};
+const onSortFilterClick = (evt) => {
+  if (evt.target.classList.contains('img-filters__button--active')) {
+    return
+  } else {
+    const filters = picturesSortFiltersForm.querySelectorAll('.img-filters__button');
+    
+    filters.forEach((filter) => {
+      if (filter.classList.contains('img-filters__button--active')) {
+        filter.classList.remove('img-filters__button--active');
+      }
+    });
+    
+    evt.target.classList.add('img-filters__button--active');
+    
+    switch(evt.target.id) {
+      case 'filter-default' : 
+        console.log('default');
+        break;
+      case 'filter-random' : 
+        console.log('random');
+        break;
+      case 'filter-discussed' : 
+        console.log('discussed');
+        break;
+    }
+  }
+}
+
+const addSortFiltersListener = () => {
+  picturesSortFiltersForm.addEventListener('click', onSortFilterClick);
+}
+
+export {pictures, gallery, renderPicures}
